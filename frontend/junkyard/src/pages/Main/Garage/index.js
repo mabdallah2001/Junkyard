a952a77c9b795e9086f6ba2b427fb6c1f99defbf
Yixun Quan <yqua7325@uni.sydney.edu.au>
2022-10-17 00:05:52 +1100

    Update index.js

--- frontend/junkyard/src/pages/Main/Garage/index.js
+++ frontend/junkyard/src/pages/Main/Garage/index.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useSearchParams } from "react-router-dom";
 import Items from "../Items/index";
-import { Grid, Typography } from '@mui/material/';
 import {toast} from "react-toastify"
+import { Container, Grid, Button, Link, Typography } from '@mui/material/';
+import CommentCard from '../../../components/CommentContainer/CommentView';
+import New from '../../Dashboard/New-Comment/new';
 
+import { useAuthController, useAppController, setRefresh } from '../../../context';
 import axios from 'axios';
 
 function Garage() {
@@ -11,6 +14,15 @@ function Garage() {
   const [data, setData] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const [garID, setGarID] = useState(parseInt(searchParams.get('id')));
+  const [authController,] = useAuthController();
+  const { user } = authController;
+
+  const [appController, appDispatch] = useAppController();
+  const { refresh } = appController;
+
+  const navigate = useNavigate();
+  const id = searchParams.get("id");
+  const [comments, setComments] = useState([]);
 
   useEffect (() => {
     let mounted = true;
@@ -25,6 +37,25 @@ function Garage() {
 
     return (() => mounted = false);
   }, []);
+
+    // TO DO: CHANGE API TO CALL COMMENT LIST FOR THIS USER ONLY
+    const fetchComments = async () => {
+      return await fetch(`http://localhost:8080/api/comments/garage/${id}`, {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(response => response.json())
+        .then(resp => setComments(resp))
+        .catch(error => console.log(error)
+        )
+    }
+  
+    useEffect(() => {
+      fetchComments();
+      if (refresh) {
+        setRefresh(appDispatch, false);
+      }
+    }, [refresh])
 
   return (
    
@@ -44,7 +75,19 @@ function Garage() {
       <Grid container item p={0} m={0} xs={10} direction="column">
         <Items />
       </Grid>
+      <br></br>
+          <Container maxWidth="lg" p={0}>
+            <Grid container spacing={3}>
+              {comments.map((item, idx) => (
+                <Grid item sm={4} md={4} key={idx}>
+                  <CommentCard data={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
     </Grid>
+    
+    
   )
 }
 export default Garage;
